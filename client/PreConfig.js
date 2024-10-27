8fea32f5adc817bbba29aba4665e7ade05ef5af3
zhouteng <251027705@qq.com>
2024-10-27 22:14:33 +0800

    🛠️ refactor(client): Update global URLs in PreConfig.js
    - Updated `window.EXPORT_URL` to use the new path `/plugins/siyuan-drawio-plugin/webapp/`.
    - Updated `window.DRAWIO_BASE_URL` to use the new path `/plugins/siyuan-drawio-plugin/webapp/`.
    - Updated `window.DRAWIO_VIEWER_URL` to use the new path `/plugins/siyuan-drawio-plugin/webapp/js/viewer.min.js`.
    - Updated `window.DRAWIO_LIGHTBOX_URL` to use the new path `/plugins/siyuan-drawio-plugin/webapp`.
    - Updated `window.DRAWIO_CONFIG` to use the new path `/plugins/siyuan-drawio-plugin/webapp/`.

--- client/PreConfig.js
+++ client/PreConfig.js
@@ -4,13 +4,13 @@
  */
 // Overrides of global vars need to be pre-loaded
 window.DRAWIO_PUBLIC_BUILD = true;
-window.EXPORT_URL = 'REPLACE_WITH_YOUR_IMAGE_SERVER';
+window.EXPORT_URL = '/plugins/siyuan-drawio-plugin/webapp/';
 window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
-window.DRAWIO_BASE_URL = null; // Replace with path to base of deployment, e.g. https://www.example.com/folder
-window.DRAWIO_VIEWER_URL = null; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
-window.DRAWIO_LIGHTBOX_URL = null; // Replace with your lightbox URL, eg. https://www.example.com
+window.DRAWIO_BASE_URL = "/plugins/siyuan-drawio-plugin/webapp/"; // Replace with path to base of deployment, e.g. https://www.example.com/folder
+window.DRAWIO_VIEWER_URL = "/plugins/siyuan-drawio-plugin/webapp/js/viewer.min.js"; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
+window.DRAWIO_LIGHTBOX_URL = "/plugins/siyuan-drawio-plugin/webapp"; // Replace with your lightbox URL, eg. https://www.example.com
 window.DRAW_MATH_URL = 'math/es5';
-window.DRAWIO_CONFIG = null; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
+window.DRAWIO_CONFIG = "/plugins/siyuan-drawio-plugin/webapp/"; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
 window.LANG = parent.siyuan.config.lang
 urlParams['sync'] = 'manual';
 // urlParams['offline'] = '0';
