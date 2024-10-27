0274b9695d66737045384be6029f4829b5ff57c0
zhouteng <251027705@qq.com>
2024-10-27 18:21:57 +0800

    🔧 chore(scripts): add prebuild-win and build-win scripts for Windows
    - Added `prebuild-win` script to run a PowerShell script for copying webapp build files.
    - Added `build-win` script to execute the build process using pnpm.
    - Removed the `copy_webapp.ps1` script as it is no longer needed.

---
+++ scripts/copy_webapp_dev.ps1
@@ -0,0 +1,50 @@
+# 源目录和目标目录
+$targetDir = "dev"
+
+$copyItems = @(
+    "images/",
+    "img/",
+    "math/",
+    "META-INF/",
+    "mxgraph/",
+    "plugins/",
+    "resources/",
+    "styles/",
+    "templates/",
+    # "clear.html",
+    # "dropbox.html",
+    # "export3.html",
+    "export-fonts.css",
+    "favicon.ico",
+    # "github.html",
+    # "gitlab.html",
+    "index.html",
+    # "monday-app-association.json",
+    # "onedrive3.html",
+    "open.html",
+    "shortcuts.svg",
+    "teams.html",
+    "vsdxImporter.html"
+)
+
+# Remove the ./$targetDir directory if it exists
+if (Test-Path "./$targetDir") {
+    Remove-Item -Path "./$targetDir" -Recurse -Force
+}
+
+# Create the ./$targetDir directory if it doesn't exist
+if (-not (Test-Path "./$targetDir")) {
+    New-Item -Path "./$targetDir" -ItemType Directory
+}
+
+foreach ($item in $copyItems) {
+    if ($item -like "*/*") {
+        # Copy directory recursively
+        Copy-Item -Path "./drawio/src/main/webapp/$item" -Destination "./$targetDir/webapp/$item" -Recurse
+    } else {
+        # Copy file
+        Copy-Item -Path "./drawio/src/main/webapp/$item" -Destination "./$targetDir/webapp/$item"
+    }
+}
+
+Write-Output "Directory copied successfully."
