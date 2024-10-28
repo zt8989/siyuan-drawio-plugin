a535d9b96055d02451b4f6b404d25e918318b00f
周腾 <233151@nbcb.com>
2024-10-28 10:12:07 +0800

    🔧 chore(scripts): 更新构建脚本和依赖项
    - 在 `package.json` 中添加了 `prebuild` 和 `postbuild` 脚本，用于在构建前后执行特定操作。
    - 新增了 `make-build-link` 脚本，用于创建构建链接。
    - 更新了 `pnpm-lock.yaml` 中的依赖版本，包括 `@types/node` 和 `sass` 等。
    - 更新了多个依赖包的版本，如 `rollup`、`vite`、`esbuild` 等，以确保项目使用最新的功能和修复。
    - 修复了 `pnpm-lock.yaml` 中的一些依赖冲突和不一致问题。

---
+++ scripts/copy_and_bundle_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Define source and destination directories
+outputDir="dist"
+webappJsDir="$outputDir/webapp/js"
+webappDir="$outputDir/webapp"
+zipFilePath="package.zip"
+
+# Define the list of files to copy
+filesToCopy=(
+    "$outputDir/PostConfig.js:$webappJsDir"
+    "$outputDir/PreConfig.js:$webappJsDir"
+    "$outputDir/service-worker.js:$webappDir"
+)
+
+# Create destination directories if they don't exist
+mkdir -p "$webappJsDir" "$webappDir"
+
+# Copy files
+for file in "${filesToCopy[@]}"; do
+    src="${file%%:*}"
+    dest="${file##*:}"
+
+    if [ -f "$src" ]; then
+        mv "$src" "$dest"
+        echo "Moved $src to $dest"
+    else
+        echo "Source file not found: $src"
+    fi
+done
+
+# Compress the contents of the directory, excluding the dist directory itself
+# (cd "$outputDir" && zip -FSr "../$zipFilePath" *)
