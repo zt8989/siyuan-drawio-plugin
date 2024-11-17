05ab0ceb6e91d4db922b0873a94b207c410938b3
zhouteng <251027705@qq.com>
2024-11-17 17:18:01 +0800

    🔧 refactor(client): Simplify and consolidate URL configurations in PreConfig.js and embed2.js
    - Removed unnecessary IIFE in PreConfig.js and restructured the code for better readability.
    - Consolidated URL configurations in embed2.js by using `window.DRAWIO_BASE_URL` for all related paths.
    - Updated `DRAWIO_LIGHTBOX_URL` in PreConfig.js to use `DRAWIO_BASE_URL` for consistency.

--- client/PreConfig.js
+++ client/PreConfig.js
@@ -1,39 +1,36 @@
 import {Workbox} from 'workbox-window';
 
-(function() {
-  if(process.env.NODE_ENV !== 'development'){
-    if ('serviceWorker' in navigator) {
-      const wb = new Workbox('service-worker.js');
-  
-      wb.register();
-    }
-  } else {
-    console.log("process.env.NODE_ENV = ", process.env.NODE_ENV)
-    urlParams['test'] = '1';
-  }
-  /**
-   * Copyright (c) 2006-2024, JGraph Ltd
-   * Copyright (c) 2006-2024, draw.io AG
-   */
-  // Overrides of global vars need to be pre-loaded
-  window.DRAWIO_PUBLIC_BUILD = true;
-  window.PLANT_URL = parent?.siyuan?.config?.editor?.plantUMLServePath ?? 'https://www.plantuml.com/plantuml/svg/~1';;
-  window.DRAWIO_BASE_URL = "/plugins/siyuan-drawio-plugin/webapp/"; // Replace with path to base of deployment, e.g. https://www.example.com/folder
-  window.DRAWIO_VIEWER_URL = "/plugins/siyuan-drawio-plugin/webapp/js/viewer.min.js"; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
-  window.DRAWIO_LIGHTBOX_URL = "/plugins/siyuan-drawio-plugin/webapp"; // Replace with your lightbox URL, eg. https://www.example.com
-  window.DRAW_MATH_URL = 'math/es5';
-  window.DRAWIO_CONFIG = null;; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
-  urlParams['sync'] = 'manual';
-  // urlParams['offline'] = '1';
-  urlParams['mode'] = 'device'
-  urlParams["gapi"]=0 //: Disables the Google integration.
-  urlParams["db"]=0 //: Disables the Dropbox integration.
-  urlParams["od"]=0 //: Disables the OneDrive integration.
-  urlParams["tr"]=0 //: Disables the Trello integration.
-  urlParams["gh"]=0 //: Disables the GitHub integration.
-  urlParams["gl"]=0 //: Disables the GitLab integration.
+if(process.env.NODE_ENV !== 'development'){
+  if ('serviceWorker' in navigator) {
+    const wb = new Workbox('service-worker.js');
 
-  if(window.parent.drawioPlugin) {
-    window.parent.drawioPlugin?.preConfig(window)
+    wb.register();
   }
-})()
+} else {
+  console.log("process.env.NODE_ENV = ", process.env.NODE_ENV)
+  urlParams['test'] = '1';
+}
+/**
+ * Copyright (c) 2006-2024, JGraph Ltd
+ * Copyright (c) 2006-2024, draw.io AG
+ */
+// Overrides of global vars need to be pre-loaded
+window.DRAWIO_PUBLIC_BUILD = true;
+window.PLANT_URL = parent?.siyuan?.config?.editor?.plantUMLServePath ?? 'https://www.plantuml.com/plantuml/svg/~1';;
+window.DRAWIO_BASE_URL = "/plugins/siyuan-drawio-plugin/webapp"; // Replace with path to base of deployment, e.g. https://www.example.com/folder
+window.DRAWIO_LIGHTBOX_URL = window.DRAWIO_BASE_URL; // Replace with your lightbox URL, eg. https://www.example.com
+window.DRAW_MATH_URL = 'math/es5';
+window.DRAWIO_CONFIG = null;; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
+urlParams['sync'] = 'manual';
+// urlParams['offline'] = '1';
+urlParams['mode'] = 'device'
+urlParams["gapi"]=0 //: Disables the Google integration.
+urlParams["db"]=0 //: Disables the Dropbox integration.
+urlParams["od"]=0 //: Disables the OneDrive integration.
+urlParams["tr"]=0 //: Disables the Trello integration.
+urlParams["gh"]=0 //: Disables the GitHub integration.
+urlParams["gl"]=0 //: Disables the GitLab integration.
+
+if(window.parent.drawioPlugin) {
+  window.parent.drawioPlugin?.preConfig(window)
+}
