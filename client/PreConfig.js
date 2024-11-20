9510b489b3ed9fba7a57cc8dec4a5a3926de1340
周腾 <233151@nbcb.com>
2024-11-20 08:30:06 +0800

    🔧 refactor(client): reformat PreConfig.js for better readability
    - Adjusted indentation and formatting to improve code readability.
    - Ensured consistent use of braces and spacing.
    - Moved conditional blocks to improve logical flow.

--- client/PreConfig.js
+++ client/PreConfig.js
@@ -1,60 +1,60 @@
 import {Workbox} from 'workbox-window';
 
-if(process.env.NODE_ENV !== 'development'){
-  if ('serviceWorker' in navigator) {
-    const wb = new Workbox('service-worker.js');
+  if(process.env.NODE_ENV !== 'development'){
+    if ('serviceWorker' in navigator) {
+      const wb = new Workbox('service-worker.js');
 
-    wb.register();
-  }
-} else {
-  urlParams['test'] = '1';
-}
-/**
- * Copyright (c) 2006-2024, JGraph Ltd
- * Copyright (c) 2006-2024, draw.io AG
- */
-// Overrides of global vars need to be pre-loaded
-function getLang(){
-  let lang = parent?.siyuan.config.lang
-
-  if (lang != null)
-    {
-      var dash = lang.indexOf('_');
-      
-      if (dash >= 0)
-      {
-        lang = lang.substring(0, dash);
-      }
-      
-      lang = lang.toLowerCase();
+      wb.register();
     }
+  } else {
+    urlParams['test'] = '1';
+  }
+  /**
+   * Copyright (c) 2006-2024, JGraph Ltd
+   * Copyright (c) 2006-2024, draw.io AG
+   */
+  // Overrides of global vars need to be pre-loaded
+  function getLang(){
+    let lang = parent?.siyuan.config.lang
 
-    return lang
-}
-window.DRAWIO_PUBLIC_BUILD = true;
-window.PLANT_URL = parent?.siyuan?.config?.editor?.plantUMLServePath ?? 'https://www.plantuml.com/plantuml/svg/~1';;
-window.DRAWIO_BASE_URL = "/plugins/siyuan-drawio-plugin/webapp/"; // Replace with path to base of deployment, e.g. https://www.example.com/folder
-window.DRAWIO_VIEWER_URL = "/plugins/siyuan-drawio-plugin/webapp/js/viewer.min.js"; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
-window.DRAWIO_LIGHTBOX_URL = "/plugins/siyuan-drawio-plugin/webapp"; // Replace with your lightbox URL, eg. https://www.example.com
-window.DRAW_MATH_URL = 'math/es5';
-window.DRAWIO_CONFIG = null;; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
-urlParams['sync'] = 'manual';
-// urlParams['offline'] = '1';
-urlParams['mode'] = 'device'
-urlParams["gapi"]=0 //: Disables the Google integration.
-urlParams["db"]=0 //: Disables the Dropbox integration.
-urlParams["od"]=0 //: Disables the OneDrive integration.
-urlParams["tr"]=0 //: Disables the Trello integration.
-urlParams["gh"]=0 //: Disables the GitHub integration.
-urlParams["gl"]=0 //: Disables the GitLab integration.
+    if (lang != null)
+      {
+        var dash = lang.indexOf('_');
+        
+        if (dash >= 0)
+        {
+          lang = lang.substring(0, dash);
+        }
+        
+        lang = lang.toLowerCase();
+      }
 
-if(parent.siyuan) {
-  urlParams['lang'] = getLang();
-}
+      return lang
+  }
+  window.DRAWIO_PUBLIC_BUILD = true;
+  window.PLANT_URL = parent?.siyuan?.config?.editor?.plantUMLServePath ?? 'https://www.plantuml.com/plantuml/svg/~1';;
+  window.DRAWIO_BASE_URL = "/plugins/siyuan-drawio-plugin/webapp/"; // Replace with path to base of deployment, e.g. https://www.example.com/folder
+  window.DRAWIO_VIEWER_URL = "/plugins/siyuan-drawio-plugin/webapp/js/viewer.min.js"; // Replace your path to the viewer js, e.g. https://www.example.com/js/viewer.min.js
+  window.DRAWIO_LIGHTBOX_URL = "/plugins/siyuan-drawio-plugin/webapp"; // Replace with your lightbox URL, eg. https://www.example.com
+  window.DRAW_MATH_URL = 'math/es5';
+  window.DRAWIO_CONFIG = null;; // Replace with your custom draw.io configurations. For more details, https://www.drawio.com/doc/faq/configure-diagram-editor
+  urlParams['sync'] = 'manual';
+  // urlParams['offline'] = '1';
+  urlParams['mode'] = 'device'
+  urlParams["gapi"]=0 //: Disables the Google integration.
+  urlParams["db"]=0 //: Disables the Dropbox integration.
+  urlParams["od"]=0 //: Disables the OneDrive integration.
+  urlParams["tr"]=0 //: Disables the Trello integration.
+  urlParams["gh"]=0 //: Disables the GitHub integration.
+  urlParams["gl"]=0 //: Disables the GitLab integration.
 
-const urlSearchParams = new URLSearchParams(location.search)
-if(urlSearchParams.get("lightbox") === "1" && !urlSearchParams.get("toolbar-config")) {
-  urlParams["toolbar-config"] = JSON.stringify({
-    refreshBtn: {}
-  })
-} 
+  if(parent.siyuan) {
+    urlParams['lang'] = getLang();
+  }
+
+  const urlSearchParams = new URLSearchParams(location.search)
+  if(urlSearchParams.get("lightbox") === "1" && !urlSearchParams.get("toolbar-config")) {
+    urlParams["toolbar-config"] = JSON.stringify({
+      refreshBtn: {}
+    })
+  } 
