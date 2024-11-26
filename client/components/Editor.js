4148c9fdf470e55569fb6fc1a17d2b58df68baa2
周腾 <233151@nbcb.com>
2024-11-26 09:56:02 +0800

    🎨 refactor(client): integrate ThemeSetup into PostConfig
    - Added import for ThemeSetup in PostConfig.js
    - Integrated ThemeSetup into the PostConfig setup process
    - Removed commented-out theme code from Editor.js

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -1,7 +1,3 @@
 export function setup() {
     Editor.enableNativeCipboard = true
-
-    // Editor.themes.push("simple");
-    // Editor.themes.push("sketch");
-    // Editor.themes.push("atlas");
 }
