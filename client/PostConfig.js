af9f1e09aeba5feaabe9959ce5b1339b79de1e88
zhouteng <251027705@qq.com>
2024-12-11 21:00:03 +0800

    🔧 refactor(Menus): Extract menu initialization logic into separate component
    - Moved menu initialization logic from `PostConfig.js` to a new file `Menus.js`.
    - Added a new `setup` function in `Menus.js` to handle menu initialization.
    - Removed redundant menu initialization code from `PostConfig.js`.
    - Improved code organization by separating concerns.

--- client/PostConfig.js
+++ client/PostConfig.js
@@ -1,6 +1,7 @@
 import { setup as EditorSetup } from "./components/Editor"
 import { setup as EditorUiSetup } from "./components/EditorUi"
 import { setup as ThemeSetup } from "./components/Theme"
+import { setup as MenuSetup } from "./components/Menus"
     /**
      * Copyright (c) 2006-2024, JGraph Ltd
      * Copyright (c) 2006-2024, draw.io AG
@@ -88,9 +89,6 @@ import { setup as ThemeSetup } from "./components/Theme"
             return { success: false };
         }
 
-        function loadFile(app, url) {
-            app.loadFile("U" + encodeURIComponent(url), true)
-        }
         //#endregion
     
         // #region App 
@@ -222,101 +220,7 @@ import { setup as ThemeSetup } from "./components/Theme"
   
     
         //#region Menus
-        var menusInit = Menus.prototype.init;
-        Menus.prototype.init = function()
-        {
-            menusInit.apply(this, arguments);
-    
-            var editorUi = this.editorUi;
-    
-            // editorUi.actions.put('useOffline', new Action(mxResources.get('useOffline') + '...', function()
-            // {
-            //     editorUi.openLink('https://www.draw.io/')
-            // }));
-    
-            // 不需要复制链接，建议直接使用`/`命令
-            // editorUi.actions.put("copyLink", new Action(parent.drawioPlugin.i18n.copyAsSiYuanLink, function() {
-            //     var file = editorUi.getCurrentFile();
-            //     electron.sendMessage("copyLink", file.getTitle())
-            // }),)
-
-            editorUi.actions.put("open",  new Action(mxResources.get('open'), function() {
-                electron.sendMessage("open", null, "open" + (new Date).getTime(), (url, name) => {
-                    loadFile(editorUi, url)
-                })
-            }),)
-            
-            // this.put('openRecent', new Menu(function(menu, parent)
-            // {
-            //     var recent = editorUi.getRecent();
-    
-            //     if (recent != null)
-            //     {
-            //         for (var i = 0; i < recent.length; i++)
-            //         {
-            //             (function(entry)
-            //             {
-            //                 menu.addItem(entry.title, null, function()
-            //                 {
-            //                     function doOpenRecent()
-            //                     {
-            //                         //Simulate opening a file via args
-            //                         editorUi.loadArgs({args: [entry.id]});
-            //                     };
-                                
-            //                     var file = editorUi.getCurrentFile();
-                                
-            //                     if (file != null && file.isModified())
-            //                     {
-            //                         editorUi.confirm(mxResources.get('allChangesLost'), null, doOpenRecent,
-            //                             mxResources.get('cancel'), mxResources.get('discardChanges'));
-            //                     }
-            //                     else
-            //                     {
-            //                         doOpenRecent();
-            //                     }
-            //                 }, parent);
-            //             })(recent[i]);
-            //         }
-    
-            //         menu.addSeparator(parent);
-            //     }
-    
-            //     menu.addItem(mxResources.get('reset'), null, function()
-            //     {
-            //         editorUi.resetRecent();
-            //     }, parent);
-            // }));
-            
-            // Replaces file menu to replace openFrom menu with open and rename downloadAs to export
-            this.put('file', new Menu(mxUtils.bind(this, function(menu, parent)
-            {
-                this.addMenuItems(menu, ['new', 'open', 'copyLink'], parent);
-                this.addSubmenu('openRecent', menu, parent);
-                this.addMenuItems(menu, ['-', /*'synchronize', '-'*/, 'save', 'saveAs', '-', 'import'], parent);
-                this.addSubmenu('exportAs', menu, parent);
-                menu.addSeparator(parent);
-                this.addSubmenu('embed', menu, parent);
-                menu.addSeparator(parent);
-                this.addMenuItems(menu, ['newLibrary', 'openLibrary'], parent);
-    
-                var file = editorUi.getCurrentFile();
-                
-                if (file != null && editorUi.fileNode != null)
-                {
-                    var filename = (file.getTitle() != null) ?
-                        file.getTitle() : editorUi.defaultFilename;
-                    
-                    if (!/(\.html)$/i.test(filename) &&
-                        !/(\.svg)$/i.test(filename))
-                    {
-                        this.addMenuItems(menu, ['-', 'properties']);
-                    }
-                }
-                
-                this.addMenuItems(menu, ['-', 'pageSetup', 'print', '-', 'close', '-', 'exit'], parent);
-            })));
-        };
+        MenuSetup(electron)
         //#endregion
     
         //#region Editor
